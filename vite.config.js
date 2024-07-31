c45a443347835ca080abd91ca02f517001124b76
siman chuadhary <chaudharysimon4@gmail.com>
2024-07-31 18:07:48 +0545

    initialized vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // ...
+  base: "/TodoList-App/",
   server: {
     mimeTypes: {
-      base: "/TodoList-App/",
       '.jsx': 'text/javascript',
     },
   },
