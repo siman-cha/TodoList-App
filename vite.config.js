40841061da591a27a67437922876af1f451e1c3d
siman chuadhary <chaudharysimon4@gmail.com>
2024-07-31 22:05:11 +0545

    initialized vite

--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  //...
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: 'src/main.jsx',
-      output: {
-        entryFileNames: 'main.js',
-        chunkFileNames: 'chunks/[name].js',
-        assetFileNames: 'assets/[name].[ext]',
-      },
-    },
+    //...
+    assetsInlineLimit: 0,
   },
-  // Add this configuration to generate a static website
-  plugins: [
-    {
-      name: 'static-website',
-      generateBundle(options, bundle) {
-        const html = `
-          <!DOCTYPE html>
-          <html lang="en">
-            <head>
-              <meta charset="UTF-8" />
-              <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-              <title>iTask - Manage your todos at one place</title>
-            </head>
-            <body>
-              <div id="root"></div>
-              <script src="main.js"></script>
-            </body>
-          </html>
-        `;
-        this.emitFile({
-          type: 'asset',
-          fileName: 'index.html',
-          source: html,
-        });
-      },
-    },
-  ],
 });
