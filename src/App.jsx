e4ac3f00ac69172e17914b76d1a51235091bc05e
siman chuadhary <chaudharysimon4@gmail.com>
2024-08-01 08:07:21 +0545

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-// import React from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom/client'
 import { useState, useEffect } from "react";
 import Navbar from "./components/navbar";
 import { FaEdit } from "react-icons/fa";
