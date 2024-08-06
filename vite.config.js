5348d6ea1c6fa543714f6a45423102d9327ee776
siman-cha <chaudharysimon4@gmail.com>
2024-08-06 17:57:37 +0545

    deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  //...
-  build: {
-    //...
-    assetsInlineLimit: 0,
-  },
-});
+  plugins: [react()],
+  base: "/TodoList-App/"
+})
